5ea5e02a05bd7ac59b38baeb9a3fa0fe4a7e7652
SalimTyagi <mdsalim10040@gmail.com>
2025-02-07 17:43:27 +0530

    Remove exceptions files

--- src/main/java/org/data/services/LoginService.java
+++ src/main/java/org/data/services/LoginService.java
@@ -5,6 +5,6 @@ import org.data.entities.Employee;
 public interface LoginService {
 
     boolean validateLogin(String userName, String password);
-    Employee getLoggedInEmployee();  // ✅ Retrieve the logged-in user
-    void setLoggedInEmployee(Employee employee); // ✅ Store the logged-in user
+    Employee getLoggedInEmployee();
+    void setLoggedInEmployee(Employee employee);
 }
