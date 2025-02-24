548b0440f00391e141c7716348efb5c8d0f37799
SalimTyagi <mdsalim10040@gmail.com>
2025-02-24 17:46:16 +0530

    search functionality added

--- src/main/java/org/pages/SearchEmployee.java
+++ src/main/java/org/pages/SearchEmployee.java
@@ -1,19 +1,20 @@
 package org.pages;
 
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.data.entities.Employee;
 import org.data.services.EmployeeService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SearchEmployee {
     @Property
     private String searchQuery;
     @Property
+    @Persist
     private List<Employee> searchResults;
     @Inject
     private EmployeeService employeeService;
@@ -23,11 +24,6 @@ public class SearchEmployee {
     private Form searchForm;
     void onActivateFromSearchForm() {
         searchResults = null;
-    }
-    void onValidateFromSearchForm() {
-        if (searchQuery == null || searchQuery.isEmpty()) {
-            searchResults = new ArrayList<>();
-        }
     }
     void onSuccessFromSearchForm() {
         searchResults = employeeService.searchEmployees(searchQuery);
