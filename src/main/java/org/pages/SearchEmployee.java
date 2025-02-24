2e8d5151db23725823e7affbdbe12e340cf84334
SalimTyagi <mdsalim10040@gmail.com>
2025-02-24 17:47:49 +0530

    search functionality added

--- src/main/java/org/pages/SearchEmployee.java
+++ src/main/java/org/pages/SearchEmployee.java
@@ -8,6 +8,7 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import org.data.entities.Employee;
 import org.data.services.EmployeeService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchEmployee {
@@ -24,6 +25,11 @@ public class SearchEmployee {
     private Form searchForm;
     void onActivateFromSearchForm() {
         searchResults = null;
+    }
+    void onValidateFromSearchForm() {
+        if (searchQuery == null || searchQuery.isEmpty()) {
+            searchResults = new ArrayList<>();
+        }
     }
     void onSuccessFromSearchForm() {
         searchResults = employeeService.searchEmployees(searchQuery);
